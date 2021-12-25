18fb8ee791f2ebe370d6e8358e8315f330bf4dcd
Deven Shetty <devenshetty123@gmail.com>
2021-12-25 16:37:23 +0530

    performed prop drilling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import FeedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    console.log("App", id);
+  };
+
   return(<div>
     <Header />
     <div className="container">
-      <FeedbackList feedback = {feedback} />
+      <FeedbackList feedback = {feedback} handleDelete={deleteFeedback} />
     </div>
   </div>)
 }
