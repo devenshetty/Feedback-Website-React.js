5272cf5d43faed2d6ecac51887d4e349cfcc41aa
Deven Shetty <devenshetty123@gmail.com>
2021-12-25 16:54:51 +0530

    implemented a delete function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    console.log("App", id);
+    if(window.confirm("Are you sure you would like to delete this feedback?")){
+      setFeedback(feedback.filter((item) => item.id !== id ))
+    }
   };
 
   return(<div>
