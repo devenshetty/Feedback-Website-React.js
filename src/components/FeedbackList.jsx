597efde47ee302fc1c13597140586c0da35b3ad2
Deven Shetty <devenshetty123@gmail.com>
2021-12-28 12:19:58 +0530

    added fade effect on feedback addition and deletion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList({feedback, handleDelete }) {
   if(!feedback || feedback.length === 0){
@@ -6,11 +7,23 @@ function FeedbackList({feedback, handleDelete }) {
   }
   else{
     return <div className="feedback-list">
+    <AnimatePresence>
     {feedback.map((item) => (
+      <motion.div key={item.id} initial= {{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} >
       <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete} />
+      </motion.div>
     ))
     }
+    </AnimatePresence>
     </div>
+
+    //below code is for a version without animation. Just uncomment the below block and remove the above code from else block.
+    // return <div className="feedback-list">
+    // {feedback.map((item) => (
+    //   <FeedbackItem key={item.id} item={item} handleDelete= {handleDelete} />
+    // ))
+    // }
+    // </div>
   }
 }
 
