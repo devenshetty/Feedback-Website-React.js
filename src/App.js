4889a816bd78e80b5973d085b05c0c91d6f68ef9
Deven Shetty <devenshetty123@gmail.com>
2021-12-28 11:54:02 +0530

    bug fix: clearing the input box after submitting the feedback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -14,10 +15,15 @@ function App() {
     }
   };
 
+  const addFeedback = (newFeedback) =>{
+      newFeedback.id = uuidv4();
+      setFeedback([newFeedback,...feedback])
+  }
+
   return(<div>
     <Header />
     <div className="container">
-      <FeedbackForm />
+      <FeedbackForm handleAdd = {addFeedback} />
       <FeedbackStats feedback = {feedback} />
       <FeedbackList feedback = {feedback} handleDelete={deleteFeedback} />
     </div>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
-function FeedbackForm(){
+function FeedbackForm({ handleAdd }){
   const [text, setText] = useState("");
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
+
+
 
   const handleTextChange = (e) => {
     if(text===""){
@@ -14,21 +18,36 @@ function FeedbackForm(){
     }
     else if(text!=="" && text.trim().length < 10){
       setMessage("Text must be atleast 10 characters");
-      setBtnDisabled(true)
+      setBtnDisabled(true);
     }
     else{
       setMessage(null);
-      setBtnDisabled=false;
+      setBtnDisabled(false);
     }
       setText(e.target.value);
   }
 
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if(text.trim().length >= 10){
+        const newFeedback = {
+          text: text,
+          rating: rating
+        }
+        handleAdd(newFeedback);
+        setText("");
+      }
+  }
+
+
+
   return (<Card>
-    <form>
+    <form onSubmit={handleSubmit}>
       <h3>  Please rate our service. </h3>
+      <RatingSelect select={(rating) => setRating(rating)} />
       <div className="input-group">
-        <input onChange={handleTextChange} type="text" placeholder="Write a review" />
-          <Button type="submit" isDisabled={btnDisabled}> post </Button> {/*change the styling later*/}
+        <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text} />
+          <Button type="submit" isDisabled={btnDisabled}> post </Button>
       </div>
       {message && <div className="message">{message}</div>}
     </form>
