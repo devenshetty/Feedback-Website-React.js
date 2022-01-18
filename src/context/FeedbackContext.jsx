f2aa671e83def9044f01f7096736d648d8469e13
Deven Shetty <devenshetty123@gmail.com>
2022-01-18 16:03:09 +0530

    fetched data from json-server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,21 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-    id:1,
-    text: "This message is from the context that I created",
-    rating: 3
-    }
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item:{},
     edit:false
   });
+
+  useEffect(() => {
+    fetchFeedback()
+  }, []);
+
+
+  //fetching the feedback
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc");
+
+    const data = await response.json();
+    setFeedback(data);
+  }
 
   const deleteFeedback = (id) => {
     if(window.confirm("Are you sure you would like to delete this feedback?")){
